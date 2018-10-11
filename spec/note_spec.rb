89e86a7af3862537e998367861b261a21cc048c7
Clare Jolly <clarejollyuk@gmail.com>
2018-10-11 22:06:09 +0100

    tidying doubles on note spec

--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -2,9 +2,10 @@ require 'note'
 
 describe 'Note' do
   describe 'Set up' do
+    let(:notebook)        { double :notebook, :title => "Title", :all_notes => [] }
     it 'create note' do
-      note = Note.new(Notebook.new("Notebook"), "title", "body")
-      expect(note.title).to eq "title"
+      note = Note.new(notebook,"this is a note title", "this is a note body")
+      expect(notebook.all_notes[0].title).to eq "this is a note title"
     end
   end
 end
